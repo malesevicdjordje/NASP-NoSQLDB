bae5c97744013b15bee1589e6ffcb487b1fb8691
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-03 14:51:36 +0100

    Added evaluation for size (m) and number of hash functions (k)

--- system/structures/bloomFilter.go
+++ system/structures/bloomFilter.go
@@ -1,6 +1,9 @@
 package structures
 
-import "hash"
+import (
+	"hash"
+	"math"
+)
 
 type BloomFilter struct {
 	Set         []byte        // set sa bitovima
@@ -9,4 +12,14 @@ type BloomFilter struct {
 	M           uint          // duzina (velicina) bloom filtera
 	P           float64       // verovatnoca false-positive
 	TimeSeconds uint          // vreme u sekundama od 1.1.1970
+}
+
+// Generise velicinu bloom filtera
+func EvaluateM(numOfElements int, falsePositiveRate float64) uint {
+	return uint(math.Ceil(float64(numOfElements) * math.Abs(math.Log(falsePositiveRate)) / math.Pow(math.Log(2), float64(2))))
+}
+
+// Generise broj hash funkcija
+func EvaluateK(numOfElements int, sizeOfFilter uint) uint {
+	return uint(math.Ceil((float64(sizeOfFilter) / float64(numOfElements)) * math.Log(2)))
 }
