352ddd3f8334ccf7d4c753ea08a88efccc6c3592
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 23:01:25 +0100

    Init Engine

--- system/Engine.go
+++ system/Engine.go
@@ -13,3 +13,15 @@ type Engine struct {
 	TokenBucket *structures.RateLimiter
 	Config      *config.Config
 }
+
+func (e *Engine) Init() {
+	e.Config = config.GetSystemConfig()
+	e.Wal = structures.NewWriteAheadLog(structures.WalPath)
+	e.memTable = structures.NewMemoryTable(e.Config.MemTableParameters.SkipListMaxHeight,
+		uint(e.Config.MemTableParameters.MaxMemTableSize),
+		uint(e.Config.MemTableParameters.MemTableThreshold))
+	e.cache = structures.NewLRUCache(e.Config.CacheParameters.CacheMaxData)
+	e.lsm = structures.NewLSMTree(e.Config.LSMParameters.LSMMaxLevel, e.Config.LSMParameters.LSMLevelSize)
+	rate := int64(e.Config.TokenBucketParameters.TokenBucketInterval)
+	e.TokenBucket = structures.NewRateLimiter(rate, e.Config.TokenBucketParameters.TokenBucketMaxTokens)
+}
