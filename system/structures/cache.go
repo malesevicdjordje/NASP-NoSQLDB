91dcce5c13a00abdb9783a9c56497d5ae430b52d
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 13:21:51 +0100

    Added Put method and his helper functions

--- system/structures/cache.go
+++ system/structures/cache.go
@@ -43,3 +43,72 @@ func NewLRUCache(maxSize int) *LRUCache {
 		values: values,
 	}
 }
+
+func (cache *LRUCache) Put(key string, value []byte) {
+	list := cache.list
+	node := NewCacheNode(key, value)
+
+	if existingValue, exists := cache.values[node.Key]; exists {
+		// Update existing node
+		cache.updateExistingNode(node, existingValue)
+		return
+	}
+
+	// Add new node
+	cache.values[node.Key] = node.Value
+
+	if list.size == list.maxSize {
+		// Evict the least recently used node
+		cache.evictLRUNode()
+	}
+
+	// Add the new node to the head of the list
+	cache.addNodeToHead(node)
+}
+
+// Helper functions
+
+func (cache *LRUCache) updateExistingNode(node *CacheNode, existingValue []byte) {
+	// Update the value in the map
+	cache.values[node.Key] = node.Value
+
+	if list := cache.list; node.Key != list.head.Key {
+		// Move the existing node to the head of the list
+		cache.moveNodeToHead(node.Key)
+	}
+}
+
+func (cache *LRUCache) evictLRUNode() {
+	list := cache.list
+
+	// Remove the tail node
+	delete(cache.values, list.tail.Key)
+
+	if list.size > 1 {
+		// Update the tail pointer
+		list.tail.Previous.Next = nil
+		list.tail = list.tail.Previous
+	} else {
+		// Reset the list when only one node is present
+		list.head = nil
+		list.tail = nil
+	}
+
+	list.size--
+}
+
+func (cache *LRUCache) addNodeToHead(node *CacheNode) {
+	list := cache.list
+
+	// Add the new node to the head of the list
+	if list.head == nil {
+		list.head = node
+		list.tail = node
+	} else {
+		node.Next = list.head
+		list.head.Previous = node
+		list.head = node
+	}
+
+	list.size++
+}
