a2e7e23617ca2788e3a197e91a236eaa42d92f3f
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 16:28:17 +0100

    Reads summary header information and checks if the target key is between start and end key.

--- system/structures/summary.go
+++ system/structures/summary.go
@@ -1,6 +1,10 @@
 package structures
 
-import "os"
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+)
 
 func FindSummaryByKey(targetKey, filename string) (found bool, offset int64) {
 	found = false
@@ -11,5 +15,50 @@ func FindSummaryByKey(targetKey, filename string) (found bool, offset int64) {
 		panic(err)
 	}
 	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	fileLenBytes := make([]byte, 8)
+	_, err = reader.Read(fileLenBytes)
+	if err != nil {
+		panic(err)
+	}
+	fileLen := binary.LittleEndian.Uint64(fileLenBytes)
+
+	startKeyLenBytes := make([]byte, 8)
+	_, err = reader.Read(startKeyLenBytes)
+	if err != nil {
+		panic(err)
+	}
+	startKeyLen := binary.LittleEndian.Uint64(startKeyLenBytes)
+
+	startKeyBytes := make([]byte, startKeyLen)
+	_, err = reader.Read(startKeyBytes)
+	if err != nil {
+		panic(err)
+	}
+	firstKey := string(startKeyBytes[:])
+
+	if targetKey < firstKey {
+		return false, 0
+	}
+
+	endKeyLenBytes := make([]byte, 8)
+	_, err = reader.Read(endKeyLenBytes)
+	if err != nil {
+		panic(err)
+	}
+	endKeyLen := binary.LittleEndian.Uint64(endKeyLenBytes)
+
+	endKeyBytes := make([]byte, endKeyLen)
+	_, err = reader.Read(endKeyBytes)
+	if err != nil {
+		panic(err)
+	}
+	lastKey := string(endKeyBytes[:])
+
+	if targetKey > lastKey {
+		return false, 0
+	}
 
 }
