e090119a96274f4cebb7bf0142057931e42edb86
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 14:21:48 +0100

    Added functions for converting

--- system/structures/merkleTree.go
+++ system/structures/merkleTree.go
@@ -1,6 +1,9 @@
 package structures
 
-import "encoding/hex"
+import (
+	"crypto/sha1"
+	"encoding/hex"
+)
 
 // MerkleRoot represents the root of the Merkle tree.
 type MerkleRoot struct {
@@ -20,4 +23,18 @@ type MerkleNode struct {
 
 func (n *MerkleNode) String() string {
 	return hex.EncodeToString(n.HashValue[:])
+}
+
+// CalculateHash calculates the SHA-1 hash of the given data.
+func CalculateHash(data []byte) [20]byte {
+	return sha1.Sum(data)
+}
+
+// ConvertStringsToBytes converts an array of strings to a 2D byte slice.
+func ConvertStringsToBytes(strings []string) [][]byte {
+	byteData := make([][]byte, len(strings))
+	for i, str := range strings {
+		byteData[i] = []byte(str)
+	}
+	return byteData
 }
