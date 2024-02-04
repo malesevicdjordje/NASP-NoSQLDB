49d933ce7983fd41fdb65f9d89557254ff9874aa
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 16:05:18 +0100

    Added function for writing to file

--- system/structures/index.go
+++ system/structures/index.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/binary"
 	"log"
+	"math/rand"
 	"os"
 )
 
@@ -99,5 +100,80 @@ func (index *SimpleIndex) Search(key string, startOffset int64) (found bool, dat
 		}
 	}
 
+	return
+}
+
+// WriteToFile writes the index data to a file and returns keys and offsets.
+func (index *SimpleIndex) WriteToFile() (keys []string, offsets []uint) {
+	currentOffset := uint(0)
+	file, err := os.Create(index.FileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	bytesLen := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytesLen, uint64(len(index.Keys)))
+	bytesWritten, err := writer.Write(bytesLen)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	currentOffset += uint(bytesWritten)
+
+	err = writer.Flush()
+	if err != nil {
+		return
+	}
+
+	rangeKeys := make([]string, 0)
+	rangeOffsets := make([]uint, 0)
+	sampleKeys := make([]string, 0)
+	sampleOffsets := make([]uint, 0)
+	for i := range index.Keys {
+		key := index.Keys[i]
+		offset := index.Offsets[i]
+		if i == 0 || i == (len(index.Keys)-1) {
+			rangeKeys = append(rangeKeys, key)
+			rangeOffsets = append(rangeOffsets, currentOffset)
+		} else if rand.Intn(100) > 50 {
+			sampleKeys = append(sampleKeys, key)
+			sampleOffsets = append(sampleOffsets, currentOffset)
+		}
+		bytes := []byte(key)
+
+		keyLen := uint64(len(bytes))
+		bytesLen := make([]byte, 8)
+		binary.LittleEndian.PutUint64(bytesLen, keyLen)
+		bytesWritten, err := writer.Write(bytesLen)
+		if err != nil {
+			log.Fatal(err)
+		}
+		currentOffset += uint(bytesWritten)
+
+		bytesWritten, err = writer.Write(bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		currentOffset += uint(bytesWritten)
+
+		bytes = make([]byte, 8)
+		binary.LittleEndian.PutUint64(bytes, uint64(offset))
+		bytesWritten, err = writer.Write(bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		currentOffset += uint(bytesWritten)
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return
+	}
+
+	keys = append(rangeKeys, sampleKeys...)
+	offsets = append(rangeOffsets, sampleOffsets...)
 	return
 }
