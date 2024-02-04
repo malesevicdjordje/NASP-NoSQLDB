a24733f50ce8780d4640484232a3f90cafd26f05
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 14:01:12 +0100

    Added Get, Delete, Print methods and removeNode helper function (with errors) 2

--- system/structures/cache.go
+++ system/structures/cache.go
@@ -46,62 +46,6 @@ func NewLRUCache(maxSize int) *LRUCache {
 	}
 }
 
-func (cache *LRUCache) Put(key string, value []byte) {
-	list := cache.list
-	node := NewCacheNode(key, value)
-
-	if existingValue, exists := cache.values[node.Key]; exists {
-		// Update existing node
-		cache.updateExistingNode(node, existingValue)
-		return
-	}
-
-	// Add new node
-	cache.values[node.Key] = node.Value
-
-	if list.size == list.maxSize {
-		// Evict the least recently used node
-		cache.evictLRUNode()
-	}
-
-	// Add the new node to the head of the list
-	cache.addNodeToHead(node)
-}
-
-func (cache *LRUCache) Get(key string) (bool, []byte) {
-	if value, exists := cache.values[key]; exists {
-		// Move accessed node to the head of the list
-		cache.moveNodeToHead(key)
-		return true, value
-	}
-	return false, nil
-}
-
-func (cache *LRUCache) Delete(key string) bool {
-	if value, exists := cache.values[key]; exists {
-		// Remove node from the list
-		cache.removeNode(key, value)
-		return true
-	}
-	return false
-}
-
-func (cache *LRUCache) Print() {
-	list := cache.list
-	fmt.Println("\nLinked List:")
-
-	current := list.head
-	for current != nil {
-		fmt.Println(current.Key)
-		current = current.Next
-	}
-
-	fmt.Println("\nMap:")
-	for key, value := range cache.values {
-		fmt.Printf("Key: %s, Value: %s\n", key, string(value))
-	}
-}
-
 // Helper functions
 
 func (cache *LRUCache) updateExistingNode(node *CacheNode, existingValue []byte) {
@@ -205,5 +149,61 @@ func (cache *LRUCache) removeNode(key string, value []byte) {
 		}
 
 		current = current.Next
+	}
+}
+
+func (cache *LRUCache) Put(key string, value []byte) {
+	list := cache.list
+	node := NewCacheNode(key, value)
+
+	if existingValue, exists := cache.values[node.Key]; exists {
+		// Update existing node
+		cache.updateExistingNode(node, existingValue)
+		return
+	}
+
+	// Add new node
+	cache.values[node.Key] = node.Value
+
+	if list.size == list.maxSize {
+		// Evict the least recently used node
+		cache.evictLRUNode()
+	}
+
+	// Add the new node to the head of the list
+	cache.addNodeToHead(node)
+}
+
+func (cache *LRUCache) Get(key string) (bool, []byte) {
+	if value, exists := cache.values[key]; exists {
+		// Move accessed node to the head of the list
+		cache.moveNodeToHead(key)
+		return true, value
+	}
+	return false, nil
+}
+
+func (cache *LRUCache) Delete(key string) bool {
+	if value, exists := cache.values[key]; exists {
+		// Remove node from the list
+		cache.removeNode(key, value)
+		return true
+	}
+	return false
+}
+
+func (cache *LRUCache) Print() {
+	list := cache.list
+	fmt.Println("\nLinked List:")
+
+	current := list.head
+	for current != nil {
+		fmt.Println(current.Key)
+		current = current.Next
+	}
+
+	fmt.Println("\nMap:")
+	for key, value := range cache.values {
+		fmt.Printf("Key: %s, Value: %s\n", key, string(value))
 	}
 }
