71e9aef63d39e7ca6e3a2c77ce4e1b935dde289d
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 18:09:16 +0100

    Added functions for Append and Persist

--- system/structures/writeAheadLog.go
+++ system/structures/writeAheadLog.go
@@ -1,6 +1,13 @@
 package structures
 
-import "hash/crc32"
+import (
+	"bufio"
+	"fmt"
+	"hash/crc32"
+	"log"
+	"os"
+	"strconv"
+)
 
 const (
 	WalPath         = "./system/data/wal/"
@@ -30,4 +37,40 @@ func (s *WalSegment) Index() uint64 {
 
 func (s *WalSegment) Data() []byte {
 	return s.data
+}
+
+func (s *WalSegment) AppendData(elemData []byte) int {
+	for i := 0; i < len(elemData); i++ {
+		if s.size >= s.capacity {
+			return i
+		}
+		s.data = append(s.data, elemData[i])
+		s.size++
+	}
+	return -1
+}
+
+func (s *WalSegment) Persist(walPath string) {
+	path := walPath + "wal" + strconv.FormatUint(s.index, 10) + ".log"
+	newFile, _ := os.Create(path)
+	err := newFile.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	file, err := os.OpenFile(path, os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	bufferedWriter := bufio.NewWriter(file)
+	err = bufferedWriter.Flush()
+	if err != nil {
+		return
+	}
+	_, err = bufferedWriter.Write(s.data)
+	err = bufferedWriter.Flush()
+	if err != nil {
+		return
+	}
 }
