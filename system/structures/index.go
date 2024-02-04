b2531cf99a8a5d5b35cb2d178d77a21a64db8967
malesevicdjordje <malesevic.sv3.2021@uns.ac.rs>
2024-02-04 16:02:36 +0100

    Added function for search index

--- system/structures/index.go
+++ system/structures/index.go
@@ -1,5 +1,12 @@
 package structures
 
+import (
+	"bufio"
+	"encoding/binary"
+	"log"
+	"os"
+)
+
 // Index defines an interface for basic indexing operations.
 type Index interface {
 	Add(key string, offset uint)
@@ -29,4 +36,68 @@ func NewSimpleIndex(keys []string, offsets []uint, fileName string) *SimpleIndex
 func (index *SimpleIndex) Add(key string, offset uint) {
 	index.Keys = append(index.Keys, key)
 	index.Offsets = append(index.Offsets, offset)
+}
+
+// Search finds a key in the index and returns its existence status and data offset.
+func (index *SimpleIndex) Search(key string, startOffset int64) (found bool, dataOffset int64) {
+	found = false
+	dataOffset = 0
+
+	file, err := os.Open(index.FileName)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	bytes := make([]byte, 8)
+	_, err = reader.Read(bytes)
+	if err != nil {
+		log.Panic(err)
+	}
+	fileLen := binary.LittleEndian.Uint64(bytes)
+
+	_, err = file.Seek(startOffset, 0)
+	if err != nil {
+		return false, 0
+	}
+
+	reader = bufio.NewReader(file)
+
+	var i uint64
+	for i = 0; i < fileLen; i++ {
+		bytes := make([]byte, 8)
+		_, err = reader.Read(bytes)
+		if err != nil {
+			log.Panic(err)
+		}
+		keyLen := binary.LittleEndian.Uint64(bytes)
+
+		bytes = make([]byte, keyLen)
+		_, err = reader.Read(bytes)
+		if err != nil {
+			log.Panic(err)
+		}
+		nodeKey := string(bytes[:])
+
+		if nodeKey == key {
+			found = true
+		} else if nodeKey > key {
+			return false, 0
+		}
+
+		bytes = make([]byte, 8)
+		_, err = reader.Read(bytes)
+		if err != nil {
+			log.Panic(err)
+		}
+		newOffset := binary.LittleEndian.Uint64(bytes)
+
+		if found {
+			dataOffset = int64(newOffset)
+			break
+		}
+	}
+
+	return
 }
